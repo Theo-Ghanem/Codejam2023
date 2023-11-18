595ef012641eed12e1a30ba80f301e8f0da753da
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2023-11-17 20:39:08 -0500

    finished routing?

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,23 +4,46 @@ import {LoginComponent} from "./pages/login/login.component";
 import {MeetingsComponent} from "./pages/meetings/meetings.component";
 import {TestsComponent} from "./pages/tests/tests.component";
 import {AssignmentsComponent} from "./pages/assignments/assignments.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: 'meetings',
-    component: MeetingsComponent
+    component: MeetingsComponent,
+    children: [
+      {
+        path: 'meeting/:id',
+        loadComponent: () => import('./pages/meetings/meeting/meeting.component').then(m => m.MeetingComponent)
+      }
+    ]
   },
   {
     path: 'tests',
-    component: TestsComponent
+    component: TestsComponent,
+    children: [
+      {
+        path: 'test/:id',
+        loadComponent: () => import('./pages/tests/test/test.component').then(m => m.TestComponent)
+      }
+    ]
   },
   {
     path: 'assignments',
-    component: AssignmentsComponent
+    component: AssignmentsComponent,
+    children: [
+      {
+        path: 'assignment/:id',
+        loadComponent: () => import('./pages/assignments/assignment/assignment.component').then(m => m.AssignmentComponent)
+      }
+    ]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   },
   {
     path: '',

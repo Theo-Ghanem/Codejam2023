4614c3cb05e68cca786c2038dc826f581558adef
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2023-11-17 20:17:09 -0500

    partial routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {MeetingsComponent} from "./pages/meetings/meetings.component";
+import {TestsComponent} from "./pages/tests/tests.component";
+import {AssignmentsComponent} from "./pages/assignments/assignments.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'meetings',
+    component: MeetingsComponent
+  },
+  {
+    path: 'tests',
+    component: TestsComponent
+  },
+  {
+    path: 'assignments',
+    component: AssignmentsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

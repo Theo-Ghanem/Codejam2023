d1029d75953713f181dc19555d76e94e91929e65
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2023-11-17 19:21:57 -0500

    basic angular app

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

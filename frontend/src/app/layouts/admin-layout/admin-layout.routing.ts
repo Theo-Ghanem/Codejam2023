8cf48ae4529af8fc34c71d0af978395e1f2d7ef5
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2023-11-18 14:09:25 -0500

    test section works

--- frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -9,7 +9,6 @@ import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import { TestComponent } from 'app/tests/test/test.component';
-import { TestComponentRenderer } from '@angular/core/testing';
 import { TestsComponent } from 'app/tests/tests.component';
 
 export const AdminLayoutRoutes: Routes = [

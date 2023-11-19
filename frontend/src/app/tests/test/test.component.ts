0df6cf9231f0994ff97e312f487887b77494b6b5
sehr-m <58871345+sehr-m@users.noreply.github.com>
2023-11-18 21:25:39 -0500

    fixes

--- frontend/src/app/tests/test/test.component.ts
+++ frontend/src/app/tests/test/test.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import {FileUploadHandlerEvent, FileUploadModule} from "primeng/fileupload";
+import {FileSendEvent, FileUploadHandlerEvent, FileUploadModule} from "primeng/fileupload";
 import {CardModule} from "primeng/card";
 import {TimelineModule} from "primeng/timeline";
 import {FormsModule} from "@angular/forms";
@@ -12,13 +12,13 @@ import {CheckboxModule} from "primeng/checkbox";
 import {DialogModule} from "primeng/dialog";
 import {animate, query, stagger, style, transition, trigger} from "@angular/animations";
 import { TopicItem } from 'app/model/topic-item';
-import { MenuItem } from 'primeng/api';
-import { MenuModule } from 'primeng/menu';
+import { GradedItem } from 'app/model/graded-item';
+import { ApiService } from 'app/services/api.service';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [CommonModule, FileUploadModule, MenuModule, CardModule, TimelineModule, FormsModule, InputTextModule, DividerModule, AccordionModule, ButtonModule, CheckboxModule, DialogModule],
+  imports: [CommonModule, FileUploadModule, CardModule, TimelineModule, NgFor, FormsModule, InputTextModule, DividerModule, AccordionModule, ButtonModule, CheckboxModule, DialogModule],
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
   encapsulation: ViewEncapsulation.None,
@@ -34,33 +34,37 @@ import { MenuModule } from 'primeng/menu';
     ])
   ]
 })
+
+
 export class TestComponent implements OnInit{
   topics: TopicItem[] = [];
-  grade: number = 50;
-  weight: number = 50;
   completedColor: string = '#34a224';
   incompleteColor: string = '#FF9800';
   showPopup: boolean = false;
-  editModeOn:boolean = false;
   selectedTopic: TopicItem = new TopicItem();
-  editMenu: MenuItem[] = [
-    {
-        label: 'Edit',
-        icon: 'pi pi-pencil',
-        command: () => {
-            this.editModeOn = true;
-        }
-    },
-    {
-        label: 'Delete',
-        icon: 'pi pi-trash',
-        command: () => {
-            this.removeTopic(this.selectedTopic);
-        }
-    }
-];
+  grades: number[] = [];
+  grade?: number = 50;
+  weight?: number = 50;
+  uploadedFiles: string[] = []
+  // grade = this.grades[0];
+  // weight = this.grades[1];
+  
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
+    this.apiService.getGrade().subscribe((data: GradedItem) => {
+      // this.grades = data;
+      console.log(data);
+      this.grade = data.grade;
+      this.weight = data.weight;
+      // this.name = data.name;
+      // if (data.grade != null, data.weight != null){
+
+      // }
+      // this.grade = data.grade;
+      // this.weight = data.weight;
+    });
+
     this.topics = [
       { title: 'Lecture 1', icon: 'pi pi-book', completed: false},
       { title: 'Lecture 2', icon: 'pi pi-book', completed: false},
@@ -83,16 +87,27 @@ export class TestComponent implements OnInit{
   }
 
   showNotes(topic: TopicItem) {
+    console.log("filenaem is " + topic.file);
+    this.apiService.getQuestions(topic.file).then((data: any) => {
+      console.log(data);
+      topic.notes = data['questions'];
+    });
     this.showPopup = true;
     this.selectedTopic = topic;
   }
 
-  onUpload(event: FileUploadHandlerEvent) {
-    this.selectedTopic.file = event.files[0];
+  onUpload(event: FileSendEvent, topic: TopicItem) {
+    console.log("i am here" + topic.title);
+    const item = event.formData.get('demo[]')
+    if (item instanceof File){
+      topic.file = item.name;
+    }
+    console.log(topic.file);
   }
 
   openEditMenu(event: any, menu: any) {
     event.stopPropagation();
     menu.toggle(event);
   }
+  
 }
